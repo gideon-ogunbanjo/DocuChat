9df7a22fd86ffe2fba34e0bde6d6234651d1feaa
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2024-06-26 11:43:44 +0100

    made minor changes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# DocuChat
-DocuChat - PDF Converser
+# DocuChat - PDF Chat App
+DocuChat is an AI-powered chatbot application that allows users to upload PDF documents and interact with them by asking questions. Built using Streamlit, LangChain, and OpenAI's GPT-3, DocuChat efficiently extracts information from PDFs and provides accurate answers to user queries.
+
+## Key Features
+- PDF Upload: Easily upload PDF documents for analysis.
+- Text Extraction: Automatically extracts text from the uploaded PDFs.
+- Text Splitting: Splits extracted text into manageable chunks for efficient processing.
+- Embeddings and Indexing: Generates embeddings using OpenAI and creates a FAISS index for fast similarity searches.
+- Interactive Q&A: Ask questions about the content of the PDF and get accurate responses powered by GPT-3.
+## Technologies Used
+- Streamlit: For building the user interface.
+- PyPDF2: For reading and extracting text from PDF documents.
+- LangChain: For managing text splitting and document processing.
+- FAISS: For performing efficient similarity searches.
+- OpenAI GPT-3: For generating responses to user queries.
+## How to Use
+- Upload a PDF: Click on the "Upload your PDF" button to upload your PDF document.
+- Ask Questions: Enter your questions in the text input field.
+- Get Answers: Receive accurate answers based on the content of your PDF.
+## Author
+Made by Gideon Ogunbanjo
---
+++ app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+import faiss
+
+# Sidebar contents
+with st.sidebar:
+    st.title('💬 DocuChat')
+    st.markdown('''
+    ## About
+    DocuChat is an AI-powered chatbot application that allows users to upload PDF documents and interact with them by asking questions.
+    This app was built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+
+    ''')
+    add_vertical_space(5)
+    st.write('Built by [Gideon Ogunbanjo](https://gideonogunbanjo.netlify.app/)')
+
+load_dotenv()
+
+def main():
+    st.header("💬 DocuChat - PDF Chat App")
+
+    # Upload a PDF file
+    pdf = st.file_uploader("Upload your PDF, ask questions and get answers.", type='pdf')
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+
+        # Embeddings
+        store_name = pdf.name[:-4]
+        st.write(f'{store_name}')
+
+        if os.path.exists(f"{store_name}.index") and os.path.exists(f"{store_name}_chunks.pkl"):
+            # Load FAISS index and chunks
+            index = faiss.read_index(f"{store_name}.index")
+            with open(f"{store_name}_chunks.pkl", "rb") as f:
+                loaded_chunks = pickle.load(f)
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS(embedding=embeddings, index=index, texts=loaded_chunks)
+            VectorStore.index = index
+            st.write('Embeddings Loaded from Disk')
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            faiss.write_index(VectorStore.index, f"{store_name}.index")
+            with open(f"{store_name}_chunks.pkl", "wb") as f:
+                pickle.dump(chunks, f)
+
+        # Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file:")
+
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+
+if __name__ == '__main__':
+    main()

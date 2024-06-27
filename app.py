ad61449a0289de6b8e2b62b69fb315ad63b9e9e0
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2024-06-27 21:44:05 +0100

    switched to google gemini

--- app.py
+++ app.py
@@ -1,74 +1,84 @@
+# Libraries
 import streamlit as st
-from dotenv import load_dotenv
-import pickle
 from PyPDF2 import PdfReader
-from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.callbacks import get_openai_callback
 import os
-import faiss
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
 
 load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def main():
-    st.header("💬 DocuChat - PDF Chat App")
-    st.markdown(" DocuChat is an AI-powered research oriented chatbot application that allows users to upload PDF documents and interact with them by asking questions.")
-
-    # Upload a PDF file
-    pdf = st.file_uploader("Upload your PDF, ask questions and get answers.", type='pdf')
-
-    if pdf is not None:
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
-
-        text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
+    return text
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text=text)
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-        # Embeddings
-        store_name = pdf.name[:-4]
-        st.write(f'{store_name}')
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
-        if os.path.exists(f"{store_name}.index") and os.path.exists(f"{store_name}_chunks.pkl"):
-            # Load FAISS index and chunks
-            index = faiss.read_index(f"{store_name}.index")
-            with open(f"{store_name}_chunks.pkl", "rb") as f:
-                loaded_chunks = pickle.load(f)
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS(embedding=embeddings, index=index, texts=loaded_chunks)
-            VectorStore.index = index
-            st.write('Embeddings Loaded from Disk')
-        else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            faiss.write_index(VectorStore.index, f"{store_name}.index")
-            with open(f"{store_name}_chunks.pkl", "wb") as f:
-                pickle.dump(chunks, f)
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    the provided context just say, "I'm sorry, the answer is not available in the context you have provided", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
 
-        # Accept user questions/query
-        query = st.text_input("Ask questions:")
+    Answer:
+    """
 
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
-            llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
-    st.write('Built by [Gideon Ogunbanjo](https://gideonogunbanjo.netlify.app/)')
+    return chain
 
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
 
-if __name__ == '__main__':
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}, return_only_outputs=True
+    )
+
+    print(response)
+    st.write("Output: ", response["output_text"])
+
+def main():
+    st.set_page_config("💬 DocuChat")
+    st.header("💬 DocuChat - PDF Chat Research Tool")
+    pdf_docs = st.file_uploader("Upload your PDF, click on the submit button, ask questions and get answers.", accept_multiple_files=True)
+    if st.button("Submit & Process"):
+        with st.spinner("Processing..."):
+            raw_text = get_pdf_text(pdf_docs)
+            text_chunks = get_text_chunks(raw_text)
+            get_vector_store(text_chunks)
+            st.success("Done")
+
+    user_question = st.text_input("Ask a Question:")
+
+    if user_question:
+        user_input(user_question)
+
+if __name__ == "__main__":
     main()
